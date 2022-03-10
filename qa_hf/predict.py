0b908b5ab8ce65fba8d688f30d3d79598c60df83
Tao Li <tli@cs.utah.edu>
2022-03-09 22:03:37 -0800

    updated outdated dependencies and potential fix for BERT rerun results

--- qa_hf/predict.py
+++ qa_hf/predict.py
@@ -176,10 +176,8 @@ def load_model(opt):
 def forward(opt, m, tok_idx, att_mask, type_idx):
 	# use type_idx only when using pre-trained bert-large
 	#	Other models either do not accept type_idx, or we trained them without it (which turns out doesn't matter much to test F1)
-	if opt.hf_model == 'bert-large-uncased-whole-word-masking-finetuned-squad':
-		start_scores, end_scores = m(tok_idx, att_mask, type_idx)
-	else:
-		start_scores, end_scores = m(tok_idx, att_mask)
+	output = m(tok_idx, att_mask, type_idx)
+	start_scores, end_scores = output.start_logits, output.end_logits
 	return torch.nn.functional.softmax(start_scores, dim=-1), torch.nn.functional.softmax(end_scores, dim=-1)
 
 
